c2fa86c3508d4a9363687a0f2721e4a1d9d642a5
KhalidMujahid <binkhalid267@gmail.com>
2025-09-27 19:05:37 -0700

    Added Alive:1

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
+const keepAlive = require("./keepAlive");
 const flash = require("connect-flash");
 const MongoStore = require("connect-mongo");
 const path = require("path");
@@ -53,6 +54,8 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).render("error", { error: err });
 });
+
+keepAlive();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
